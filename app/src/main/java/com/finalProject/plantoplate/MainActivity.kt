4359ad823e6095b0f2682d7ca46eec2e7fabc923
mexican-wolf-trance <erin@popily.com>
2020-05-02 14:21:13 -0500

    adding functionality to the buttons with onclick listeners

--- app/src/main/java/com/finalProject/plantoplate/MainActivity.kt
+++ app/src/main/java/com/finalProject/plantoplate/MainActivity.kt
@@ -2,11 +2,33 @@ package com.finalProject.plantoplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        my_recipes_btn.setOnClickListener()
+        {
+
+        }
+
+        my_profile_btn.setOnClickListener()
+        {
+
+        }
+
+        share_btn.setOnClickListener()
+        {
+
+        }
+
+        plan_btn.setOnClickListener()
+        {
+
+        }
     }
 }
