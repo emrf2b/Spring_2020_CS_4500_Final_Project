b15119ef33545fe0320f74045b248ab698ddf05e
mexican-wolf-trance <bargain.ghost@yandex.com>
2020-05-03 08:17:31 -0500

    attempting to create a spinner, but my god why is everything associated with android so goddam complicated?

--- app/src/main/java/com/finalProject/plantoplate/PlansFragment.kt
+++ app/src/main/java/com/finalProject/plantoplate/PlansFragment.kt
@@ -1,5 +1,6 @@
 package com.finalProject.plantoplate
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -7,18 +8,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_plans.*
 import kotlinx.android.synthetic.main.fragment_plans.view.*
 
 
 class PlansFragment : Fragment()
 {
+    @SuppressLint("ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View?
-    {
+    ): View? {
         Log.e("TAG", "Hit the frag")
         val view = inflater.inflate(R.layout.fragment_plans, container, false)
 
@@ -29,6 +34,72 @@ class PlansFragment : Fragment()
             //finish()
         }
 
+        val numbers = resources.getStringArray(R.array.meal_no)
+        val types = resources.getStringArray(R.array.meal_type)
+        val noSpinner: Spinner? = getView()?.findViewById(R.id.meal_number_spinner)
+        val typeSpinner: Spinner? = getView()?.findViewById(R.id.meal_type_spinner)
+
+        if (noSpinner != null) {
+            val noAdapter = activity?.let {
+                ArrayAdapter.createFromResource(
+                    it,
+                    R.id.meal_number_spinner,
+                    android.R.layout.simple_spinner_item
+                )
+                    .also { adapter -> adapter.setDropDownViewResource(android.R.layout.spinner_custom_layout) }
+            }
+            noSpinner.adapter = noAdapter
+        }
+
+        if (typeSpinner != null)
+        {
+            val typeAdapter = activity?.let {
+                ArrayAdapter.createFromResource(
+                    it,
+                    R.id.meal_type_spinner,
+                    android.R.layout.simple_spinner_item
+                )
+                    .also { adapter -> adapter.setDropDownViewResource(android.R.layout.spinner_custom_layout) }
+            }
+            typeSpinner.adapter = typeAdapter
+        }
+
+        if (noSpinner != null)
+        {
+            noSpinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener
+            {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                )
+                { Toast.makeText(activity,getString(R.string.selected_item) + " " +"" + types[position], Toast.LENGTH_SHORT).show() }
+
+                override fun onNothingSelected(parent: AdapterView<*>)
+                {
+                    // write code to perform some action
+                }
+            }
+        }
+
+        if (typeSpinner != null)
+        {
+            typeSpinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener
+            {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                )
+                { Toast.makeText(activity,getString(R.string.selected_item) + " " +"" + numbers[position], Toast.LENGTH_SHORT).show() }
+
+                override fun onNothingSelected(parent: AdapterView<*>)
+                {
+                    //Empty
+                }
+            }
+        }
         return view
     }
+
 }
